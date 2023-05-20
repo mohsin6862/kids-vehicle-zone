4319fd85bf2b6394906c0573d62cc8ab46003297
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-20 16:59:54 +0600

    Shop by category updated

--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -4,7 +4,7 @@ import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 import car from '../../../assets/image/collector.jpeg';
-import truck from '../../../assets/image/Truck2.jpeg'; 
+import truck from '../../../assets/image/Truck2.jpeg';
 import collector from '../../../assets/image/car2.jpg';
 
 const ShopByCategory = () => {
@@ -53,70 +53,124 @@ const ShopByCategory = () => {
           </TabList>
 
           <TabPanel className='w-full flex justify-center'>
-            <div className='flex flex-col items-center'>
+            <div className='grid grid-cols-2 gap-4 mt-8'>
               <div className='relative'>
                 <img
-                  className='w-72 h-60 rounded-lg hover:scale-105 transition-all duration-300 mt-6 sm:mt-16'
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
                   src={car}
                   alt=''
                 />
-                <div className='absolute top-0 left-0 right-0 bottom-0 bg-blue-500 bg-opacity-30 opacity-0 hover:opacity-100 transition-all duration-300 rounded-lg'></div>
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/cars'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                  New Arrival
+                </Link>
               </div>
-              <Link
-                to='/cars'
-                className='mt-4 text-lg font-semibold text-blue-500 hover:text-blue-700'
-              >
-                View Cars
-              </Link>
+              <div className='relative'>
+                <img
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
+                  src={car}
+                  alt=''
+                />
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/allvehicles'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                  All Cars
+                </Link>
+              </div>
             </div>
           </TabPanel>
           <TabPanel className='flex justify-center'>
-            <div className='flex flex-col items-center'>
+            <div className='grid grid-cols-2 gap-4 mt-8'>
               <div className='relative'>
                 <img
-                  className='w-72 h-60 rounded-lg hover:scale-105 transition-all duration-300 mt-6 sm:mt-16'
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
                   src={truck}
                   alt=''
                 />
-                <div className='absolute top-0 left-0 right-0 bottom-0 bg-blue-500 bg-opacity-30 opacity-0 hover:opacity-100 transition-all duration-300 rounded-lg'></div>
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/trucks'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                 New Arrival
+                </Link>
               </div>
-              <Link
-                to='/trucks'
-                className='mt-4 text-lg font-semibold text-blue-500 hover:text-blue-700'
-              >
-                View Trucks
-              </Link>
+              <div className='relative'>
+                <img
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
+                  src={truck}
+                  alt=''
+                />
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/allvehicles'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                  All Trucks
+                </Link>
+              </div>
             </div>
           </TabPanel>
           <TabPanel className='flex justify-center'>
-            <div className='flex flex-col items-center'>
+            <div className='grid grid-cols-2 gap-4 mt-8'>
               <div className='relative'>
                 <img
-                  className='w-72 h-60 rounded-lg hover:scale-105 transition-all duration-300 mt-6 sm:mt-16'
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
                   src={collector}
                   alt=''
                 />
-                <div className='absolute top-0 left-0 right-0 bottom-0 bg-blue-500 bg-opacity-30 opacity-0 hover:opacity-100 transition-all duration-300 rounded-lg'></div>
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/collectors'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                 New Arrival
+                </Link>
               </div>
-              <Link
-                to='/collectors'
-                className='mt-4 text-lg font-semibold text-blue-500 hover:text-blue-700'
-              >
-                View Collectors
-              </Link>
+              <div className='relative'>
+                <img
+                  className='w-full h-60 rounded-lg hover:scale-105 transition-all duration-300'
+                  src={collector}
+                  alt=''
+                />
+                <div className='absolute top-0 left-0 right-0 bottom-0 rounded-lg'>
+                  <div className='bg-gradient-to-b from-transparent to-black opacity-70 h-full rounded-lg'></div>
+                </div>
+                <Link
+                  to='/allvehicles'
+                  className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
+                >
+                  All Collectors
+                </Link>
+              </div>
             </div>
           </TabPanel>
         </Tabs>
       </div>
 
       {/* Additional Features */}
-     
+
       <div className='flex justify-center mt-8'>
         <Link
           to='/allvehicles'
-          className='px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600'
+          className='px-6 py-3 text-lg font-semibold  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) '
         >
-          View All Vehicle
+          View All Vehicles
         </Link>
       </div>
     </div>
